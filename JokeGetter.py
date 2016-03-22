19314a7efe3886eb887da3709c9bfdfd6fcab0c9
hardbird <hardbird@126.com>
2016-03-22 09:21:57 +0800

    修改html标记bug

--- JokeGetter.py
+++ JokeGetter.py
@@ -10,23 +10,23 @@ from utils import get_settings
 
 
 class JokeGetter(object):
-	def __init__(self):
+        def __init__(self):
 
-		self.dir_name = os.path.dirname(os.path.abspath((__file__)))
-		self.filename = os.path.join(self.dir_name, 'settings.yaml')
-		self.settings = get_settings(self.filename)
+                self.dir_name = os.path.dirname(os.path.abspath((__file__)))
+                self.filename = os.path.join(self.dir_name, 'settings.yaml')
+                self.settings = get_settings(self.filename)
 
-		self.url = self.settings['API']['URL']
-		self.apikey = self.settings['API']['APIKEY']
-		
+                self.url = self.settings['API']['URL']
+                self.apikey = self.settings['API']['APIKEY']
 
-	def get_jokelist(self):
 
-		req = urllib2.Request(self.url)
-		req.add_header("apikey", self.apikey)
-		resq = urllib2.urlopen(req)
-		content = resq.read()
-		if(content):
-			contentlist = json.loads(content)['showapi_res_body']['contentlist']
-			jokelist = [(x['title'], x['text']) for x in contentlist]
-			return jokelist
+        def get_jokelist(self):
+
+                req = urllib2.Request(self.url)
+                req.add_header("apikey", self.apikey)
+                resq = urllib2.urlopen(req)
+                content = resq.read()
+                if(content):
+                        contentlist = json.loads(content)['showapi_res_body']['contentlist']
+                        jokelist = [(x['title'], x['text'].replace('<p>', '\n').replace('</p>', '')) for x in contentlist]
+                        return jokelist
